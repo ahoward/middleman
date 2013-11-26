1194dd3305954927152c5a440a9f28c4ab76f865
Róman Joost <rjoost@redhat.com>
2013-11-26 23:47:02 +1000

    Bootstrapping middleman app
    The app will be used to coordinate work.

---
+++ middleman/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Middleman::Application.config.secret_token = '33e8800c61c836be9f568b6a16d39264996388261f3824cef918aa8e5ce07106401e62f715ef7c721fe47ed32317eebf3987a1198d4fba85b67e02896cf701cd'
