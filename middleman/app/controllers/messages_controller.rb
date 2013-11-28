b7fe1604095c4303bec928071c7804a9a37eae43
Róman Joost <rjoost@redhat.com>
2013-11-29 04:02:50 +1000

    Don't create duplicated messages

--- middleman/app/controllers/messages_controller.rb
+++ middleman/app/controllers/messages_controller.rb
@@ -8,9 +8,13 @@ class MessagesController < ApplicationController
 
   def create
     bug = Bug.from_string(params[:bug])
-    peer = Peer.find_by_email(params[:email])
-    peer ||= Peer.create(:email => params[:email])
-    message = Message.create(:text => params[:text], :peer => peer, :bug => bug)
+    if bug.message.nil?
+      peer = Peer.find_by_email(params[:email])
+      peer ||= Peer.create(:email => params[:email])
+      message = Message.create(:text => params[:text], :peer => peer, :bug => bug)
+    else
+      message = bug.message
+    end
     respond_with(message)
   end
 
--- middleman/test/functional/messages_controller_test.rb
+++ middleman/test/functional/messages_controller_test.rb
@@ -11,11 +11,19 @@ class MessagesControllerTest < ActionController::TestCase
     post :create, :format => :json, :bug => '23423'
     assert_response :unprocessable_entity
 
+    form = {
+      :format => :json,
+      :email => 'foo@bar.com',
+      :bug => '#21332',
+      :text => 'Foobar',
+    }
     assert_difference('Message.count') do
-      post :create, :format => :json,
-        :email => 'foo@bar.com',
-        :bug => '#21332',
-        :text => 'Foobar'
+      post :create, form
+    end
+    assert_response :success
+
+    assert_no_difference('Message.count') do
+      post :create, form
     end
     assert_response :success
   end
